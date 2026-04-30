5f5a6a3e9c4c1009f41799b584ef6401af4aafbc
Norberto Andres Luna <betoluna89@gmail.com>
2026-04-30 08:47:09 -0300

    [ADD] Models for character and eye_color, and their relationship

---
+++ app/models/character_model.py
@@ -0,0 +1,17 @@
+from database.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Character(Base):
+    __tablename__ = 'characters'
+    
+    id = Column(Integer, primary_key= True, index= True)
+    name = Column(String(100), nullable= False)
+    height = Column(Integer, nullable= False)
+    mass = Column(Integer, nullable= False)
+    hair_color = Column(String(100), nullable= False)
+    skin_color = Column(String(100), nullable= False)
+    eye_color_id = Column(Integer, ForeignKey('eye_colors.id'), nullable= False)
+    
+    
+    eye_color = relationship('EyeColor')
---
+++ app/models/eye_color_model.py
@@ -0,0 +1,9 @@
+from database.db import Base
+from sqlalchemy import Column, Integer, String
+
+
+class EyeColor(Base):
+    __tablename__ = 'eye_colors'
+    
+    id = Column(Integer, primary_key= True, index= True)
+    color = Column(String(100), nullable= False)
